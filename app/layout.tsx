1d1bcb0909e5cfeb15bad3050d0f7a242e190d8c
savperera369 <saviru2002@outlook.com>
2024-07-19 16:20:04 -0400

    build new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="p-5">
         <Theme>
           <NavBar />
           <main>
